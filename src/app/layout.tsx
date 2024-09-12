2ef9702570737dec168029f4694c7f1bebac4658
Adrian200727 <102123986+ByteMaverick2007@users.noreply.github.com>
2024-09-12 10:17:20 +0700

    p

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Lexend_Deca } from 'next/font/google';
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 
 const montserrat = Lexend_Deca({ subsets: ['latin'] })
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${montserrat.className} scrollbar-hide`}>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
